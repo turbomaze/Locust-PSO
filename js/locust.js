96f3f7bdf0361fd7c03b1c57d10772e3c899d3b7
Anthony Liu <anthonyscottliu@gmail.com>
2014-07-14 20:17:15 -0700

    Implements a random search algorithm in place of the PSO.

--- js/locust.js
+++ js/locust.js
@@ -32,15 +32,64 @@ var Locust = (function() {
     return { //return public functions
         /** @params
           * config   --- JSON that changes the behavior of Locust
-          * wrapper  --- object that outputs a score for a given combination
-          *              of parameters; the field over which to optimize
+          *              {
+          *                  numSteps: number of steps to run the PSO
+          *                  swarmSize: number of particles in the swarm
+          *                  params: array of [min, max] pairs, one per
+          *                          parameter
+          *              }
+          * wrapper  --- object with an 'eval' function that outputs a score
+          *              for a given combination of parameters; the field
+          *              over which to optimize
           *
           * @return
           * the best set of parameters discovered using the conditions
           * outlined in config
         **/
         optimize: function(config, wrapper) {
+            function getRandParams() {
+                var xs = [];
+                for (var ai = 0; ai < config.params.length; ai++) {
+                    xs.push(getRandFloat(
+                        config.params[ai][0], config.params[ai][1] 
+                    ));
+                } 
+                return xs;
+            }
+
+            var best = [null, -Infinity];
+            for (var ai = 0; ai < config.numSteps; ai++) {
+                var xs = getRandParams();
+                var y = wrapper.eval(xs);
+                if (y > best[1]) {
+                    best[0] = xs;
+                    best[1] = y;
+                }
+            }
             
+            return best[0];
         }
     };
 })();
+
+window.addEventListener('load', function() {
+    var paramsToOptimize = [[16, 48], [-70, 219]];
+    var field = Faucet.drip(paramsToOptimize, 1);
+    var argMax = Locust.optimize({
+        numSteps: 20,
+        swarmSize: 16,
+        params: paramsToOptimize,
+    }, field);
+    var max = field.eval(argMax);
+    console.log('Maximum possible: '+1);
+    console.log(max+' achieved with params: '+JSON.stringify(argMax));
+});
+
+
+
+
+
+
+
+
+
